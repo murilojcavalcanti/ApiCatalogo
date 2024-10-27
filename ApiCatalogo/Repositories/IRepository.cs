369f4b33dd7a6a83fa147a3d71e60fe7b4f5ebf7
murilojcavalcanti <josemurilo7@gmail.com>
2024-10-27 13:15:54 -0300

    Criando Repositorios genericos

---
+++ ApiCatalogo/Repositories/IRepository.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq.Expressions;
+
+namespace ApiCatalogo.Repositories
+{
+    /*
+     * Repositorio generico 
+     * Aqui acontece a definição da interface com todas as operações comuns as entidades
+     */
+    public interface IRepository<T>
+    {
+        //Realizar prestando atenção no principio ISP(Princípio da Segregação da Interface)
+        IEnumerable<T> GetAll();
+
+        /*Expression é usada para representar expressões lambda
+        Func é um delegate que é uma função que pode ser passada como argumento,
+        que representa uma função lambda que vai receber um objeto do tipo T e retorna um boleano, e retorna com
+        base no predicado que será o criterio usado para filtrar
+        */
+        T? Get(Expression<Func<T,bool>>predicate);
+
+        T create(T entity);
+        
+        T Update(T entity);
+
+        T Delete(T entity);
+
+
+    }
+}
---
+++ ApiCatalogo/Repositories/Repository.cs
@@ -0,0 +1,62 @@
+﻿using ApiCatalogo.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiCatalogo.Repositories
+{
+
+    /*
+     * Classe generica que vai implementar a interface Repository
+     * nesta classe também foi adicionada uma clausula para que o tipo T seja uma classe
+     */
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly AppDbContext Context;
+
+        public Repository(AppDbContext context)
+        {
+            Context = context;
+        }
+        public IEnumerable<T> GetAll()
+        {
+            return Context.Set<T>().ToList();
+            /*
+             O metodo set é usado para acessar uma coleção ou uma tabela
+             */
+        }
+        public T? Get(System.Linq.Expressions.Expression<Func<T, bool>> predicate)
+        {
+            return Context.Set<T>().FirstOrDefault(predicate);
+            /*
+             * Vai buscar o primeiro resultado que obedecer ao predicate ou retornará nulo
+             */
+        }
+
+
+        public T create(T entity)
+        {
+            Context.Set<T>().Add(entity);
+            Context.SaveChanges();
+            return entity;
+        }
+       
+
+        public T Update(T entity)
+        {
+            Context.Set<T>().Update(entity);//Indicado para atualizações completas
+            /*Context.Entry(entity).State=EntityState.Modified;
+             Define explicitamente o estado da entidade como modificado, então o entity framework vai 
+             identificar que a entidade foi alterada e gera um sql de atualização para a entidade
+             */
+            Context.SaveChanges();
+            return entity;
+        }
+
+        public T Delete(T entity)
+        {
+            Context.Set<T>().Remove(entity);
+            Context.SaveChanges();
+            return entity;
+        }
+
+    }
+}
