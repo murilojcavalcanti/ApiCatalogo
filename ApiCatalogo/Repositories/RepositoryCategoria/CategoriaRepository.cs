07ab7da73281e2e4f791e5468acaa0c2f1644495
murilojcavalcanti <josemurilo7@gmail.com>
2024-10-27 13:16:11 -0300

    Criando repositorio de categoria

---
+++ ApiCatalogo/Repositories/RepositoryCategoria/CategoriaRepository.cs
@@ -0,0 +1,16 @@
+﻿
+
+using ApiCatalogo.Context;
+using ApiCatalogo.Models;
+using System.Linq.Expressions;
+
+namespace ApiCatalogo.Repositories.RepositoryCategoria
+{
+    public class CategoriaRepository : Repository<Categoria>, ICategoriaRepository
+    {
+        //Caso a classe precise usar o context, ela irá usar a instancia de context da classe base, já injetada.
+        public CategoriaRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
