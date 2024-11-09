f1fbcf286002f0f34a2a077a14daefb3455f7139
murilojcavalcanti <josemurilo7@gmail.com>
2024-11-09 14:51:53 -0300

    Criando o Mapeador de produto

---
+++ ApiCatalogo/DTO/Mapping/Profiles/ProdutoProfile.cs
@@ -0,0 +1,15 @@
+﻿using ApiCatalogo.DTO.CategoriaDto;
+using ApiCatalogo.DTO.ProdutoDto;
+using ApiCatalogo.Models;
+using AutoMapper;
+
+namespace ApiCatalogo.DTO.Mapping.Profiles
+{
+    public class ProdutoProfile : Profile
+    {
+        public ProdutoProfile()
+        {
+            CreateMap<Produto, ProdutoDTO>().ReverseMap();
+        }
+    }
+}
