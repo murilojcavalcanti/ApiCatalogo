0afae9e9e7f305126ef118399245e5070d0c8b44
murilojcavalcanti <josemurilo7@gmail.com>
2024-11-16 13:04:51 -0300

    criando dtos

---
+++ ApiCatalogo/DTO/ProdutoDto/RequestUpdateProdutoDTO.cs
@@ -0,0 +1,28 @@
+﻿using ApiCatalogo.Models;
+using ApiCatalogo.Validation;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace ApiCatalogo.DTO.ProdutoDto;
+public class RequestUpdateProdutoDTO:IValidatableObject
+{
+    [StringLength(80, ErrorMessage = "O nome deve ter até 80 caracteres", MinimumLength = 3)]//Define o tamanho em bytes
+    [PrimeiraLetraMaiuscula]
+    public string? Nome { get; set; }
+
+
+    [Range(1,9999,ErrorMessage = "O estoque deve estar entre 1 e 9999")]
+    public float Estoque { get; set; }
+
+    public DateTime DataCadastro { get; set; }
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (DataCadastro <= DateTime.Now)
+        {
+            yield return new ValidationResult("A data de cadastro deve ser maior que a data atual!",
+                new[] { nameof(this.DataCadastro)});
+
+        }
+    }
+}
---
+++ ApiCatalogo/DTO/ProdutoDto/ResponseUpdateProdutoDTO.cs
@@ -0,0 +1,22 @@
+﻿using ApiCatalogo.Models;
+using ApiCatalogo.Validation;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace ApiCatalogo.DTO.ProdutoDto;
+public class ResponseUpdateProdutoDTO
+{
+    public int ProdutoId { get; set; }
+    public string? Nome { get; set; }
+
+    public string? Descricao { get; set; }
+
+    public decimal Preco { get; set; }
+
+    public string? ImagemUrl { get; set; }
+    public float Estoque { get; set; }
+    public DateTime DataCadastro { get; set; }
+    public int categoriaId { get; set; }
+    public Categoria? Categoria { get; set; }
+}
